1c9ce781e29603e7bd4bfe7a03fad02880787925
Evelyne Akoth <evelyneoakoth@gmail.com>
2018-03-28 13:37:01 +0300

    creat layout

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-console.log('app loaded')
+const form =document.querySelector('form')
 
-const div = document.querySelector('.container')
+const taskInput = document.querySelector('input[name=taskName]')
+const dateInput = document.querySelector('input[name=taskDate]')
+const ul = document.querySelector("ul")
 
-console.log(div)
+form.onsubmit = function(e) {
+	e.preventDefault()
+
+	let taskTitle = taskInput.value
+	let taskDate = dateInput.value
+
+	const li = document.createElement('li')
+	li.textContent = taskTitle
+	li.classList.add('list-group-item')
+	li.classList.add('d-flex')
+	li.classList.add('justify-content-between')
+	li.classList.add('align-items-center')
+
+
+	const span = document.createElement('span')
+	span.textContent = taskDate
+	span.classList.add('barge')
+	span.classList.add('badge-primary')
+	span.classList.add('barge-pill')
+
+
+	li.appendChild(span)
+
+	ul.appendChild(li)
+}
+
